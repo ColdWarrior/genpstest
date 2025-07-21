a827ebcc6d8ecb4fa76c8567153d636dc59c5f69
Allan Andrew Abundo <ColdWarrior@users.noreply.github.com>
2025-07-21 21:15:59 +0800

    Commit

--- genpstest_scheduledcallback/client/src/widgets/ScheduleCallbackWidget.js
+++ genpstest_scheduledcallback/client/src/widgets/ScheduleCallbackWidget.js
@@ -18,9 +18,9 @@
 		xhr.setRequestHeader("Accept", "application/json");
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xhr.setRequestHeader("Access-Control-Allow-Headers", "*");
-		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-		xhr.setRequestHeader('Access-Control-Allow-Credentials', true);
-		xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "https://callbacktest.abundo.dev/");
+		xhr.setRequestHeader("Access-Control-Allow-Credentials", true);
+		xhr.setRequestHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
 		
 		xhr.onload = () => {
 		  if (xhr.readyState == 4 && xhr.status == 201) {
