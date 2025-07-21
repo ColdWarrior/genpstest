2f7b822a650668ca3bcbd06bf73d5dd45a840fec
Allan Andrew Abundo <ColdWarrior@users.noreply.github.com>
2025-07-21 20:24:36 +0800

    Commit

--- genpstest_scheduledcallback/client/src/widgets/ScheduleCallbackWidget.js
+++ genpstest_scheduledcallback/client/src/widgets/ScheduleCallbackWidget.js
@@ -8,6 +8,7 @@
 
     // Schedules a callback with the PureCloud Api
     var scheduleCallback = function (scheduledCallbackData) {
+		
 		const xhr = new XMLHttpRequest();
 		xhr.open("POST", "https://api.mypurecloud.com/api/v2/conversations/callbacks");
 		xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
@@ -23,15 +24,16 @@
 			console.log(`Error: ${xhr.status}`);
 		  }
 		};
+		
 		xhr.send(body);
 		
         //return serviceApi.post("/purecloud/scheduledcallback", scheduledCallbackData)
-        .then(function (data) {
-            console.log("Created a scheduledCallback!", data);
-        })
-        .catch(function (err) {
-            console.log("No scheduledCallback for you!", err);
-        });
+        //.then(function (data) {
+        //    console.log("Created a scheduledCallback!", data);
+        //})
+        //.catch(function (err) {
+        //   console.log("No scheduledCallback for you!", err);
+        //});
     };
 
     var getBottomRightCoordinates = function (el) {
